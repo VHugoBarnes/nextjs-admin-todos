b5d5b627de5d154151e420845ac21a8c3e3ede8d
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-18 19:29:50 -0600

    Layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { WidgetItem } from "@/components";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/SidebarItem.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { CiBookmarkCheck } from "react-icons/ci";
+
+export const SidebarItem = ({ children, href }: { children: React.ReactNode, href: string }) => {
+  const path = usePathname();
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={`${path === href ? "text-white bg-gradient-to-r from-sky-600 to-cyan-400" : "text-white bg-cyan-400"} params === href ? "" : "relative px-4 py-3 flex items-center space-x-4 rounded-xl"`}
+      >
+        <CiBookmarkCheck size={30} />
+        <span className="-mr-1 font-medium">{children}</span>
+      </Link>
+    </li>
+  );
+};
