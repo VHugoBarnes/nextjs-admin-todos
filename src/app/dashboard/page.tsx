543a0bffee7d94f6afb78f52745b8a12bfd6a307
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-18 19:45:54 -0600

    metadata

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { WidgetItem } from "@/components";
 import React from "react";
 
+export const metadata = {
+  title: "Dashboard"
+};
+
 const page = () => {
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+export const metadata = {
+  title: "Rest TODOs"
+};
+
 const page = () => {
   return (
     <div>
