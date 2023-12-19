1815b15ec0354214e1112c437d6541c59054d939
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-19 17:21:22 -0600

    useOptimistic hook

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Todo } from "@prisma/client";
 import React from "react";
+import { Todo } from "@prisma/client";
 import styles from "./TodoItem.module.css";
 import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 
@@ -10,24 +10,38 @@ interface Props {
 };
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
+  const [todoOptimistic, toggleTodoOptimistic] = React.useOptimistic(
+    todo,
+    (state, newCompleteValue: boolean) => ({ ...state, complete: newCompleteValue })
+  );
+
+  const onToggle = async () => {
+    try {
+      React.startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete));
+      await toggleTodo(todoOptimistic.id, !todoOptimistic.complete);
+    } catch (error) {
+      React.startTransition(() => toggleTodoOptimistic(!todoOptimistic.complete));
+    }
+  };
+
   return (
-    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
+    <div className={todoOptimistic.complete ? styles.todoDone : styles.todoPending}>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
-          onClick={() => toggleTodo(todo.id, !todo.complete)}
+          onClick={onToggle}
           className={`
             flex p-2 rounded-md cursor-pointer
             hover:bg-opacity-60
-            ${todo.complete ? "bg-blue-100" : "bg-red-100"}
+            ${todoOptimistic.complete ? "bg-blue-100" : "bg-red-100"}
           `}
         >
           {
-            todo.complete ? <IoCheckboxOutline size={30} /> : <IoSquareOutline size={30} />
+            todoOptimistic.complete ? <IoCheckboxOutline size={30} /> : <IoSquareOutline size={30} />
           }
         </div>
 
         <div className="text-center sm:text-left">
-          {todo.description}
+          {todoOptimistic.description}
         </div>
       </div>
     </div>
