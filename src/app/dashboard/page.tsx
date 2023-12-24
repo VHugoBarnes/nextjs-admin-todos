99e279e1e135d55903d42106e9dafc3c78fdec5c
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-23 21:20:26 -0600

    Obtaining info from server side

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,26 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export const metadata = {
   title: "Dashboard"
 };
 
-const page = () => {
+const DashboardPage = async () => {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem title="x">hola</WidgetItem>
+      <WidgetItem title="Connected user server side">
+        {JSON.stringify(session.user, null, 2)}
+      </WidgetItem>
     </div>
   );
 };
 
-export default page;
+export default DashboardPage;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,8 +3,11 @@ import Link from "next/link";
 import React from "react";
 import { CiLogout } from "react-icons/ci";
 import { SidebarItem } from ".";
+import { getServerSession } from "next-auth";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession();
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -15,8 +18,16 @@ export const Sidebar = () => {
         </div>
 
         <div className="mt-8 text-center">
-          <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" alt="" width={40} height={40} priority className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+          <Image
+            src={session?.user ? session.user?.image ? session.user.image : "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" : "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"}
+            alt=""
+            width={40}
+            height={40}
+            priority
+            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+            unoptimized
+          />
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{session?.user ? session.user.name : "Cynthia J. Watts"}</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
